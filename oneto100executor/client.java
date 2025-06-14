726b38485553d8fd92503770df8684587aabc485
Tushar Shaw <128092800+STusharADS@users.noreply.github.com>
2025-06-14 20:39:39 +0530

    cachedThreadPool

--- oneto100executor/client.java
+++ oneto100executor/client.java
@@ -5,8 +5,9 @@ import java.util.concurrent.Executors;
 
 public class client {
     public static void main(String args[]){
-        ExecutorService ex= Executors.newFixedThreadPool(5);
-        for(int i=0;i<100;i++){
+        // ExecutorService ex= Executors.newFixedThreadPool(5);
+        ExecutorService ex=Executors.newCachedThreadPool();
+        for(int i=0;i<1000;i++){
             printnum obj=new printnum(i);
             ex.execute(obj);
         }
